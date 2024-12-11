0fb8dc1938ffb37a10af782735757a45a37829be
pranavchaitu <pranavchaitu676@gmail.com>
2024-12-11 23:56:18 +0530

    solved runtime-env issues

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { trpc } from "@/app/_trpc/client"
 import { db } from "@/db"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { notFound, useRouter } from "next/navigation"
@@ -10,18 +13,21 @@ interface PageProps {
 
 async function Page({ params } : PageProps) {
     const { getUser } = getKindeServerSession()
-    const user = await getUser()
     const router = useRouter()
+    let file;
+    const {mutate} = trpc.getFile.useMutation({
+        onSuccess(f) {
+            file = f
+        }
+    })
     const { fileId } = params
+    mutate({
+        key : fileId
+    })
+    const user = await getUser()
     if(!user || !user.id) {
         router.push(`/auth-callback?origin=dashboard/${fileId}`)
     }
-    const file = await db.file.findFirst({
-        where : {
-            id : fileId,
-            userId : user.id
-        }
-    })
     if(!file) return notFound()
     return <div>
         {params.fileId}
